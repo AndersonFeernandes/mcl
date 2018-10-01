5aab35fd73abe18f8e4dc4f1b06ee978d602c023
Anderson Luiz da Cunha Fernandes <anderson.fernandes@mjv.net>
2018-10-01 09:06:19 -0300

    correcao responsividade

--- src/slider/slider.js
+++ src/slider/slider.js
@@ -9,181 +9,185 @@ import Card from "./card/card";
 
 // Styles
 import {
-    Arrows,
-    LeftArrow,
-    Page,
-    PageController,
-    RightArrow,
-    SliderContent
+  Arrows,
+  LeftArrow,
+  Page,
+  PageController,
+  RightArrow,
+  SliderContent
 } from "./slider-styles";
 
 class Slider extends React.Component {
+  // Life Cycle
+  constructor(props, ctx) {
+    super(props, ctx);
+    const { maxShow } = this.props;
 
-    // Life Cycle
-    constructor(props, ctx) {
-        super(props, ctx);
-        const { maxShow } = this.props;
+    this.state = {
+      cards: [],
+      maxShow: maxShow,
+      show: false,
+      lastShow: false,
+      page: 0
+    };
 
-        this.state = {
-            cards: [],
-            maxShow: maxShow,
-            show: false,
-            lastShow: false,
-            page: 0
+    this.slider = React.createRef();
 
-        };
+    this.changePage = this.changePage.bind(this);
+    this.getCardRender = this.getCardRender.bind(this);
+  }
 
-        this.changePage = this.changePage.bind(this);
-        this.getCardRender = this.getCardRender.bind(this);
-    }
+  async componentDidMount() {
+    this.getCardRender();
+    window.addEventListener("resize", this.getCardRender);
+  }
 
-    async componentDidMount() {
-        this.getCardRender();
-        window.addEventListener("resize", this.getCardRender);
-    }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.getCardRender);
+  }
 
+  // Getters
+  async getPreviousCards() {
+    let { page } = this.state;
+    const initalPage = 0;
+    let cards;
 
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.getCardRender);
-    }
+    page--;
+    if (page < initalPage) page = initalPage;
 
-    // Getters
-    async getPreviousCards() {
-        let { page } = this.state;
-        const initalPage = 0;
-        let cards;
+    cards = await this.getCards(page);
+    return { cards: cards, page: page };
+  }
 
+  async getNextCards() {
+    let { page, lastPage, cards } = this.state;
+
+    if (!lastPage || page < lastPage) {
+      page++;
+      cards = await this.getCards(page);
+
+      if (cards.length === 0) {
         page--;
-        if (page < initalPage)
-            page = initalPage;
-
+        lastPage = page;
         cards = await this.getCards(page);
-        return { cards: cards, page: page };
+      }
     }
 
-    async getNextCards() {
-        let { page, lastPage, cards } = this.state;
+    return { cards, page, lastPage };
+  }
 
-        if (!lastPage || page < lastPage) {
-            page++;
-            cards = await this.getCards(page);
+  // Setters
+  async changePage(next) {
+    let cards;
 
-            if (cards.length === 0) {
-                page--;
-                lastPage = page;
-                cards = await this.getCards(page);
-            }
-        }
-
-        return { cards, page, lastPage };
+    if (next) {
+      cards = await this.getNextCards();
+    } else {
+      cards = await this.getPreviousCards();
     }
 
-    // Setters
-    async changePage(next) {
-        let cards;
+    this.setState({ ...this.state, ...cards });
+  }
 
-        if (next) {
-            cards = await this.getNextCards();
-        } else {
-            cards = await this.getPreviousCards();
-        }
+  // API Service
+  async getCards(page, firstShow = false) {
+    let { show } = this.state;
 
-        this.setState({ ...this.state, ...cards });
+    if (!show) {
+      show = firstShow;
     }
 
-    // API Service
-    async getCards(page, firstShow = false) {
-        let { show } = this.state;
+    const headers = new Headers({
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    });
 
-        if (!show) {
-            show = firstShow;
-        }
+    const start = page * show;
+    const end = start + show;
 
-        const headers = new Headers({
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        });
+    const response = await fetch(
+      `${API_CARD_URL}?_start=${start}&_end=${end}`,
+      {
+        method: "GET",
+        headers: headers
+      }
+    );
 
-        const start = page * show;
-        const end = start + show;
+    const cards = await response.json();
 
-        const response = await
-            fetch(`${API_CARD_URL}?_start=${start}&_end=${end}`, {
-                method: "GET",
-                headers: headers
-            });
+    return cards;
+  }
 
-        const cards = await response.json();
+  // Responsive
+  async getCardRender() {
+    let { show, maxShow, page, cards, lastShow } = this.state;
+    const { scrollWidth } = this.slider;
 
-        return cards;
+    const cardWidth = 420;
+
+    let cardShow = Math.floor(scrollWidth / cardWidth);
+
+    if (cardShow < 1) {
+      cardShow = 1;
     }
 
-    // Responsive
-    async getCardRender() {
-        const { innerWidth } = window;
-        let { show, maxShow, page, cards, lastShow } = this.state;
-
-        const cardWidth = 420;
-
-        let cardShow = Math.floor(innerWidth / cardWidth);
-
-        if (maxShow && maxShow < cardShow) {
-            cardShow = maxShow;
-        }
-
-        if (cardShow === show && lastShow !== show) {
-            lastShow = show;
-            cards = await this.getCards(page, cardShow);
-            page = 0;
-        } else if (!(cardShow === show && lastShow !== show)) {
-            lastShow = show;
-            cards = await this.getCards(page, cardShow);
-        }
-
-        this.setState({
-            show: cardShow,
-            cards: cards,
-            page: page,
-            lastShow: lastShow
-        });
+    if (maxShow && maxShow < cardShow) {
+      cardShow = maxShow;
     }
 
-    // Accessibility
-    handleAccessibility(e, next = true) {
-        if (e.key === "Enter" || e.key === " ") {
-            this.changePage(next);
-        }
+    if (cardShow === show && lastShow !== show) {
+      lastShow = show;
+      cards = await this.getCards(page, cardShow);
+      page = 0;
+    } else if (!(cardShow === show && lastShow !== show)) {
+      lastShow = show;
+      cards = await this.getCards(page, cardShow);
     }
 
-    // Render
-    render() {
-        const { cards } = this.state;
-        const { tabIndex } = this.props;
+    this.setState({
+      show: cardShow,
+      cards: cards,
+      page: page,
+      lastShow: lastShow
+    });
+  }
 
-        return (
-            <SliderContent>
-                <Page>
-                    {cards.map((card, key) =>
-                        <Card tabIndex={tabIndex} details={card} key={key} />
-                    )}
-                </Page>
-                <PageController>
-                    <Arrows>
-                        <LeftArrow
-                            tabIndex={tabIndex + 1}
-                            onKeyDown={(e) => this.handleAccessibility(e, false)}
-                            onClick={() => this.changePage(false)} />
-                        <RightArrow
-                            tabIndex={tabIndex + 1}
-                            onKeyDown={(e) => this.handleAccessibility(e, true)}
-                            onClick={() => this.changePage(true)} />
-                    </Arrows>
-                </PageController>
-            </SliderContent>
-        );
-
-
+  // Accessibility
+  handleAccessibility(e, next = true) {
+    if (e.key === "Enter" || e.key === " ") {
+      this.changePage(next);
     }
+  }
+
+  // Render
+  render() {
+    const { cards } = this.state;
+    const { tabIndex } = this.props;
+
+    return (
+      <SliderContent innerRef={div => (this.slider = div)}>
+        <Page>
+          {cards.map((card, key) => (
+            <Card tabIndex={tabIndex} details={card} key={key} />
+          ))}
+        </Page>
+        <PageController>
+          <Arrows>
+            <LeftArrow
+              tabIndex={tabIndex + 1}
+              onKeyDown={e => this.handleAccessibility(e, false)}
+              onClick={() => this.changePage(false)}
+            />
+            <RightArrow
+              tabIndex={tabIndex + 1}
+              onKeyDown={e => this.handleAccessibility(e, true)}
+              onClick={() => this.changePage(true)}
+            />
+          </Arrows>
+        </PageController>
+      </SliderContent>
+    );
+  }
 }
 
 export default Slider;
